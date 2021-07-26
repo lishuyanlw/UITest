fac322066f71c69caf09278ef431498081cfac58
sdrogers1 <Shruti.Desai1@rci.rogers.com>
2021-07-26 16:42:05 -0400

    updated code

--- src/test/java/com/tsc/test/tests/homePage/HP_TC02_Verify_TSmainImage.java
+++ src/test/java/com/tsc/test/tests/homePage/HP_TC02_Verify_TSmainImage.java
@@ -11,11 +11,17 @@ import com.tsc.test.base.BaseTest;
 		@Test(groups={"Home","Regression"})
 
 	
+<<<<<<< Upstream, based on origin/master
 	public void validateTSmainImageSection() throws IOException, InterruptedException {
 		homePageThreadLocal().closeadd();
+=======
+	public void validateTSmainImageSection() throws IOException {
+		//OnAirSectionPageThreadLocal().closeadd();
+>>>>>>> ffa51ea updated code
 	//reporter.softAssert(getglobalheaderPageThreadLocal().validateURL("https://qa-tsc.tsc.ca/"), "TSC url is correct", "TSC url is incorrect");
 	//reporter.reportLogWithScreenshot("Home Page");
 	
+<<<<<<< Upstream, based on origin/master
 	reporter.softAssert(homePageThreadLocal().verifyTSmainimglinkCount(),"TS main upper section link & image Count is: "+homePageThreadLocal().getTSmainimgCount(),"TS main upper section link & image Count is: "+homePageThreadLocal().getTSmainimgCount());
 		for(int i=0; i<homePageThreadLocal().getTSmainimgCount(); i++) {
 			reporter.softAssert(homePageThreadLocal().validateTSmainimage(i),"TS main image is present for tile: "+(i+1),"TS main image is not present for tile: "+(i+1));
@@ -31,6 +37,25 @@ import com.tsc.test.base.BaseTest;
 		for(int i=0; i<homePageThreadLocal().getTSimgCount(); i++) {
 			reporter.softAssert(homePageThreadLocal().validateTSimage(i),"TS image is present for tile: "+(i+1),"TS image is not present for tile: "+(i+1));
 			reporter.reportLog(homePageThreadLocal().validateTSimageLinks(i));
+=======
+	
+	reporter.softAssert(homePageThreadLocal().verifyTSmainimglinkCount(),"TS main section link & image Count is: "+homePageThreadLocal().getTSmainimgCount(),"TS main section link & image Count is: "+homePageThreadLocal().getTSmainimgCount());
+	 for(int i=0; i<homePageThreadLocal().getTSmainimgCount(); i++) {
+		
+		 reporter.softAssert(homePageThreadLocal().validateTSmainimage(i),"TS main image is present for tile: "+(i+1),"TS main image is not present for tile: "+(i+1));
+		 reporter.reportLog(homePageThreadLocal().validateTSmainimageLinks(i));
+
+	 	}
+	 
+	/*
+	 validateText(homePageThreadLocal().validateTSimagesection(),"Today's Showstopper TM Offers","TS Main image bottom Section");
+
+	reporter.softAssert(homePageThreadLocal().verifyTSimglinkCount(),"TS image bottom section link & image Count is: "+homePageThreadLocal().getTSimgCount(),"TS image bottom section link & image Count is: "+homePageThreadLocal().getTSimgCount());
+	 for(int i=0; i<homePageThreadLocal().getTSimgCount(); i++) {
+		
+		 reporter.softAssert(homePageThreadLocal().validateTSimage(i),"TS image is present for tile: "+(i+1),"TS image is not present for tile: "+(i+1));
+		 reporter.reportLog(homePageThreadLocal().validateTSimageLinks(i));
+>>>>>>> ffa51ea updated code
 
 	 	}*/
 	}
