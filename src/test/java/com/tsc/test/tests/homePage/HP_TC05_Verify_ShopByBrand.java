74e2cf3fd3d3680cef584974d9f3f8a03ccf4bac
Waji.Abbas <Waji.Abbas@rci.rogers.com>
2021-08-12 12:24:06 -0400

    Addition of constant file + code file and showing usage in ShopBy Brand TC

--- src/test/java/com/tsc/test/tests/homePage/HP_TC05_Verify_ShopByBrand.java
+++ src/test/java/com/tsc/test/tests/homePage/HP_TC05_Verify_ShopByBrand.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import com.tsc.test.base.BaseTest;
 
 import com.tsc.pages.base.BasePage;
+import com.tsc.data.Handler.TestDataHandler;
 
 public class HP_TC05_Verify_ShopByBrand extends BaseTest{
 	
@@ -19,30 +20,15 @@ public class HP_TC05_Verify_ShopByBrand extends BaseTest{
 	public void validateShopByBrand() throws IOException {
 				
 	reporter.softAssert(getglobalheaderPageThreadLocal().validateURL((new BasePage(this.getDriver())).getBaseURL()+"/"), "TSC url is correct", "TSC url is incorrect");
-		
 	reporter.reportLogWithScreenshot("Home Page");
-	
 	reporter.softAssert(homePageThreadLocal().validateShopByBrandAutomaticScrollingAction(),"The automatic scrolling function in ShopByBrand works well","The The automatic scrolling function in ShopByBrand doesn't work");
-	
-	validateText(homePageThreadLocal().validateShopByBrandHeaderText().toLowerCase(), "shop by brand", "<Shop By Brand> text is visible and valid");
-	
+	validateText(homePageThreadLocal().validateShopByBrandHeaderText().toUpperCase(), TestDataHandler.constantDataVariables.getlbl_ShopByBrand(), "<Shop By Brand> text is visible and valid");
 	reporter.softAssert(homePageThreadLocal().validateShopByBrandHref(),"All products in ShopByBrand have valid links","Some products in ShopByBrand have no valid links");
-	
 	reporter.softAssert(homePageThreadLocal().validateShopByBrandImg(),"All products in ShopByBrand have valid image sources","Some products in ShopByBrand have no valid image sources");
-		
 	reporter.softAssert(homePageThreadLocal().validateShopByBrandClickPrevButton(),"The Prev button in ShopByBrand works well","The Prev button in ShopByBrand doesn't work");
-	
 	reporter.softAssert(homePageThreadLocal().validateShopByBrandClickNextButton(),"The Next button in ShopByBrand works well","The Next button in ShopByBrand doesn't work");
-			
-	reporter.softAssert(homePageThreadLocal().validateShopByBrandViewAllLink(),"The ViewAll link matches designed pattern","The ViewAll link doesn't match designed pattern");
-		
-	(new BasePage(this.getDriver())).navigateToURL((new BasePage(this.getDriver())).getBaseURL());
-	
+	reporter.softAssert(homePageThreadLocal().validateShopByBrandViewAllLink(TestDataHandler.constantDataVariables.getlnk_ShopByBrandViewAll()),"The ViewAll link matches designed pattern","The ViewAll link doesn't match designed pattern");
 	reporter.softAssert(homePageThreadLocal().validateShopByBrandUrlAfterClickingViewAllLink(),"The Url after clicking ViewAll link in ShopByBrand is valid","The Url after clicking ViewAll link in ShopByBrand is invalid");
-	
-	(new BasePage(this.getDriver())).navigateToURL((new BasePage(this.getDriver())).getBaseURL());	
-	
-	
+	(new BasePage(this.getDriver())).navigateToURL((new BasePage(this.getDriver())).getBaseURL());
 	}
-	
 }
