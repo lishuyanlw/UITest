1cadbc4f76b2b2cb6d0b18d212099a9b6a500356
sdrogers1 <Shruti.Desai1@rci.rogers.com>
2021-08-12 20:56:07 -0400

    updated code

--- src/test/java/com/tsc/test/tests/homePage/HP_TC02_Verify_TSmainImage.java
+++ src/test/java/com/tsc/test/tests/homePage/HP_TC02_Verify_TSmainImage.java
@@ -11,48 +11,12 @@ import com.tsc.test.base.BaseTest;
 		@Test(groups={"Home","Regression"})
 
 	
-<<<<<<< Upstream, based on origin/master
 	public void validateTSmainImageSection() throws IOException, InterruptedException {
-<<<<<<< Upstream, based on origin/master
-		homePageThreadLocal().closeadd();
-=======
-	public void validateTSmainImageSection() throws IOException {
-		//OnAirSectionPageThreadLocal().closeadd();
->>>>>>> ffa51ea updated code
+			//homePageThreadLocal().closeadd();	
 	//reporter.softAssert(getglobalheaderPageThreadLocal().validateURL("https://qa-tsc.tsc.ca/"), "TSC url is correct", "TSC url is incorrect");
 	//reporter.reportLogWithScreenshot("Home Page");
-=======
-		//homePageThreadLocal().closeadd();
-	reporter.softAssert(getglobalheaderPageThreadLocal().validateURL((new BasePage(this.getDriver())).getBaseURL()+"/"), "TSC url is correct", "TSC url is incorrect");
-	reporter.reportLogWithScreenshot("Home Page");
->>>>>>> a99416f URL hard coded value replaced
-	
-<<<<<<< Upstream, based on origin/master
-	reporter.softAssert(homePageThreadLocal().verifyTSmainimglinkCount(),"TS main upper section link & image Count is: "+homePageThreadLocal().getTSmainimgCount(),"TS main upper section link & image Count is: "+homePageThreadLocal().getTSmainimgCount());
-		for(int i=0; i<homePageThreadLocal().getTSmainimgCount(); i++) {
-			reporter.softAssert(homePageThreadLocal().validateTSmainimage(i),"TS main image is present for tile: "+(i+1),"TS main image is not present for tile: "+(i+1));
-			reporter.reportLog(homePageThreadLocal().validateTSmainimageHref(i));
-		//	reporter.reportLog(homePageThreadLocal().validateUrlAfterClickingTSmainImageLink(i));
-			reporter.softAssert(homePageThreadLocal().validateUrlAfterClickingTSmainImageLink(i),"TS Main Image link is loaded properly","TS Main Image link is not loaded properly");
-	 	}
-	 
-	
-	validateText(homePageThreadLocal().validateTSimagesection(),"Today's Showstopper TM Offers","TS Main image bottom Section");
 
-	reporter.softAssert(homePageThreadLocal().verifyTSimglinkCount(),"TS image bottom section link & image Count is: "+homePageThreadLocal().getTSimgCount(),"TS image bottom section link & image Count is: "+homePageThreadLocal().getTSimgCount());
-		for(int i=0; i<homePageThreadLocal().getTSimgCount(); i++) {
-			reporter.softAssert(homePageThreadLocal().validateTSimage(i),"TS image is present for tile: "+(i+1),"TS image is not present for tile: "+(i+1));
-			reporter.reportLog(homePageThreadLocal().validateTSimageLinks(i));
-=======
 	
-	reporter.softAssert(homePageThreadLocal().verifyTSmainimglinkCount(),"TS main section link & image Count is: "+homePageThreadLocal().getTSmainimgCount(),"TS main section link & image Count is: "+homePageThreadLocal().getTSmainimgCount());
-	 for(int i=0; i<homePageThreadLocal().getTSmainimgCount(); i++) {
-		
-		 reporter.softAssert(homePageThreadLocal().validateTSmainimage(i),"TS main image is present for tile: "+(i+1),"TS main image is not present for tile: "+(i+1));
-		 reporter.reportLog(homePageThreadLocal().validateTSmainimageLinks(i));
-
-<<<<<<< Upstream, based on origin/master
-	 	}
 	 
 	
 	 validateText(homePageThreadLocal().validateTSimagesection(),"Today's Showstopper TM Offers","TS Main image bottom Section");
@@ -62,13 +26,8 @@ import com.tsc.test.base.BaseTest;
 		
 		 reporter.softAssert(homePageThreadLocal().validateTSimage(i),"TS image is present for tile: "+(i+1),"TS image is not present for tile: "+(i+1));
 		 reporter.reportLog(homePageThreadLocal().validateTSimageLinks(i));
->>>>>>> ffa51ea updated code
+
 
 	 	}
 	}
 }
-=======
-		}
-	}
-}
->>>>>>> a99416f URL hard coded value replaced
