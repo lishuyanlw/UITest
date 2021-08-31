8a21048ffafb8737acd02b25c3fdb13ef2abe6fa
sdrogers1 <Shruti.Desai1@rci.rogers.com>
2021-08-31 11:17:39 -0400

    FOHeading is replaced by flyoutHeading.

--- src/test/java/com/tsc/test/tests/homePage/HP_TC02_Global_Header_Verify_FlyoutHeadings.java
+++ src/test/java/com/tsc/test/tests/homePage/HP_TC02_Global_Header_Verify_FlyoutHeadings.java
@@ -11,7 +11,6 @@ import io.appium.java_client.ios.ListensToSyslogMessages;
 
 public class HP_TC02_Global_Header_Verify_FlyoutHeadings extends BaseTest {
 
-	
 		@Test(groups={"Home","Regression"})	    
 		public void verifyFlyoutHeadings() throws IOException {
 			getglobalheaderPageThreadLocal().closeadd();
@@ -19,21 +18,15 @@ public class HP_TC02_Global_Header_Verify_FlyoutHeadings extends BaseTest {
 		reporter.softAssert(getglobalheaderPageThreadLocal().validateURL(lsBaseUrl+"/"), "TSC url is correct", "TSC url is incorrect");
 		reporter.reportLogWithScreenshot("Home Page");
 		reporter.reportLog("Validating Flyouts all departments & it's URL");
-		
-		String FOHeading,lsUrl;
-		
-		for(int i=0; i<getglobalheaderPageThreadLocal().getFlyoutHeadingCount(); i++) {
-			FOHeading = getglobalheaderPageThreadLocal().getFlyoutHeadings(i);
-			lsUrl=getglobalheaderPageThreadLocal().getFlyoutLink(i);		
-			reporter.softAssert(getglobalheaderPageThreadLocal().validateFlyoutHeadings(i),"Flyout heading is present for grid "+ (i+1) ,"Flyout heading is not present for grid "+ (i+1));
-			reporter.softAssert(FOHeading, TestDataHandler.constantDataVariables.getlst_FlyoutHeading().get(i),"Flyout display " + FOHeading + " department. It's text is visible and valid","Flyout display " + FOHeading + " department. It's text is visible and valid");
-			
+		String flyoutHeading,lsUrl;
+			for(int i=0; i<getglobalheaderPageThreadLocal().getFlyoutHeadingCount(); i++) {
+				flyoutHeading = getglobalheaderPageThreadLocal().getFlyoutHeadings(i);
+				lsUrl=getglobalheaderPageThreadLocal().getFlyoutLink(i);		
+				reporter.softAssert(getglobalheaderPageThreadLocal().validateFlyoutHeadings(i),"Flyout heading is present for grid "+ (i+1) ,"Flyout heading is not present for grid "+ (i+1));
+				reporter.softAssert(flyoutHeading, TestDataHandler.constantDataVariables.getlst_FlyoutHeading().get(i),"Flyout display " + flyoutHeading + " department. It's text is visible and valid","Flyout display " + flyoutHeading + " department. It's text is visible and valid");
 			//Verify href is not empty and full url before clicking Flyout link
-			reporter.softAssert(getglobalheaderPageThreadLocal().validateFlyouthref(i),"Flyout href is present for Link of "+FOHeading,"Flyout href is not present for link of "+FOHeading);
-			reporter.softAssert(lsUrl.contains(TestDataHandler.constantDataVariables.getlst_FlyoutLink().get(i)), FOHeading + "'s URL is correct", FOHeading + "'s URL is incorrect");
-			
+				reporter.softAssert(getglobalheaderPageThreadLocal().validateFlyouthref(i),"Flyout href is present for Link of "+flyoutHeading,"Flyout href is not present for link of "+flyoutHeading);
+				reporter.softAssert(lsUrl.contains(TestDataHandler.constantDataVariables.getlst_FlyoutLink().get(i)), flyoutHeading + "'s URL is correct", flyoutHeading + "'s URL is incorrect");
 			}
 		}
-
-		
 }
