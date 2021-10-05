d11cee66c198ce465d1d91394c0c29e56019e214
Sachin.Sharma6 <sachin.sharma6@rci.rogers.ca>
2021-10-05 09:01:35 -0400

    Updated files for yml file changes

--- src/test/java/com/tsc/data/Handler/TestDataHandler.java
+++ src/test/java/com/tsc/data/Handler/TestDataHandler.java
@@ -1,9 +1,7 @@
 package com.tsc.data.Handler;
 
 import java.io.FileNotFoundException;
-import java.util.List;
-
-import org.testng.ITestNGMethod;
+import com.tsc.data.pojos.ConstantDataFile;
 
 import com.tsc.data.pojos.ConstantData;
 import com.tsc.data.pojos.SauceSettings;
@@ -11,10 +9,11 @@ import com.tsc.data.pojos.SauceSettings;
 public class TestDataHandler {
 	public static SauceSettings sauceSettings;
 	public static ConstantData constantDataVariables;
+	public static ConstantDataFile constantDataFile;
 
 	public static void dataInit() throws FileNotFoundException {
 		sauceSettings = YamlHandler.getSauceSettings("/src/test/resources/test-data/SauceSettings.yml");
 		constantDataVariables = YamlHandler.getConstantDataVariables("/src/test/resources/test-data/ConstantData.yml");
+		constantDataFile = YamlHandler.getConstantDataVariable("/src/test/resources/test-data/ConstantDataFile.yml");
 	}
-
 }
--- src/test/java/com/tsc/data/Handler/YamlHandler.java
+++ src/test/java/com/tsc/data/Handler/YamlHandler.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tsc.data.pojos.ConstantDataFile;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
@@ -29,6 +32,21 @@ public class YamlHandler {
 		inputStream = new FileInputStream(new File(System.getProperty("user.dir") + strConstantLocation));
 		ConstantData headerFooterLinks = yaml.load(inputStream);
 		return headerFooterLinks;
+	}
+
+	public static Map<String,Object> getConstantDataVariablesMap(String strConstantLocation) throws FileNotFoundException {
+		Yaml yaml = new Yaml();
+		InputStream inputStream;
+
+		inputStream = new FileInputStream(new File(System.getProperty("user.dir") + strConstantLocation));
+		return yaml.load(inputStream);
+		//return headerFooterLinks.entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toSet());
+	}
+
+	public static ConstantDataFile getConstantDataVariable(String strConstantLocation) throws FileNotFoundException {
+		Map<String, Object> dataMap = getConstantDataVariablesMap(strConstantLocation);
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.convertValue(dataMap, ConstantDataFile.class);
 	}
 
 }
