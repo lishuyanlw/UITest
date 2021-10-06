225000ca0b8b873944628188439041409495a387
Wei Li - EXT <Wei.Li@rci.rogers.ca>
2021-10-06 15:18:26 -0400

    Added Go back to English action in GF_TC02

--- src/test/java/com/tsc/test/tests/globalFooter/GF_TC02_Verify_GlobalFooter_CustomerHubLinksAndAboutTSCLinks_Language.java
+++ src/test/java/com/tsc/test/tests/globalFooter/GF_TC02_Verify_GlobalFooter_CustomerHubLinksAndAboutTSCLinks_Language.java
@@ -165,8 +165,7 @@ public class GF_TC02_Verify_GlobalFooter_CustomerHubLinksAndAboutTSCLinks_Langua
 		}
 		lsHref=basePage.getElementHref(getGlobalFooterPageThreadLocal().lnkLanguage);
 		reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,lsYmlHref),"The current Language switch href of "+lsHref+" contains "+lsYmlHref+ "in yml file","The current Language switch href of "+lsHref+" does not contain "+lsYmlHref+ "in yml file");
-			
-		
+				
 		//TSC customer hub links
 		lsText=basePage.getUTFEnabledData(basePage.getElementText(getGlobalFooterPageThreadLocal().lblTSCCustomerHubText));
 		reporter.softAssert(lsText.equalsIgnoreCase(lsText_TSCCustomerHub_Fr),"Customer Hub text French transaltion is correct","Customer Hub text French transaltion is not correct");
@@ -202,6 +201,110 @@ public class GF_TC02_Verify_GlobalFooter_CustomerHubLinksAndAboutTSCLinks_Langua
 		reporter.softAssert(lsText.equalsIgnoreCase(lsText_Copyright_Line1_Fr),"Copyright line1 French transaltion is correct","Copyright line1 French transaltion is not correct");
 		lsText=basePage.getUTFEnabledData(basePage.getElementText(getGlobalFooterPageThreadLocal().txtCopyrightLine2));
 		reporter.softAssert(lsText.equalsIgnoreCase(lsText_Copyright_Line2_Fr),"Copyright line2 French transaltion is correct","Copyright line2 French transaltion is not correct");
+		
+		//Switch back to English
+		getGlobalFooterPageThreadLocal().switchlanguage();
+		
+		//Credit card
+		lsText=basePage.getElementText(getGlobalFooterPageThreadLocal().lnkCreditCard);
+		String lsText_CreditCard_En=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsText_CreditCard_Fr);
+		reporter.softAssert(lsText.equalsIgnoreCase(lsText_CreditCard_En),"The English text of "+lsText+" is correct","The English text of "+lsText+" is not correct");
+		lsYmlHref=getGlobalFooterPageThreadLocal().getLinkWithSpecificName(lstNameAndLinks,lsText,true);
+		if(lsYmlHref.isEmpty()) {
+			reporter.reportLog("Unable to find credit card link in yml file.");
+		}
+		lsHref=basePage.getElementHref(getGlobalFooterPageThreadLocal().lnkCreditCard);
+		reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,lsYmlHref),"The current credit card href of "+lsHref+" is equal to "+lsYmlHref+ "in yml file","The current credit card href of "+lsHref+" is not equal to "+lsYmlHref+ "in yml file");
+		lsImageSrc=basePage.getElementImageSrc(getGlobalFooterPageThreadLocal().imgCreditCard);
+		reporter.softAssert(!lsImageSrc.isEmpty(),"The credit card image is not empty.","The credit card image is empty.");
+		
+		//Gift card
+		lsText=basePage.getElementText(getGlobalFooterPageThreadLocal().lnkGiftCard);
+		String lsText_GiftCard_En=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsText_GiftCard_Fr);
+		reporter.softAssert(lsText.equalsIgnoreCase(lsText_GiftCard_En),"The English text of "+lsText+" is correct","The English text of "+lsText+" is not correct");
+		lsYmlHref=getGlobalFooterPageThreadLocal().getLinkWithSpecificName(lstNameAndLinks,lsText,true);
+		if(lsYmlHref.isEmpty()) {
+			reporter.reportLog("Unable to find Gift card link in yml file.");
+		}
+		lsHref=basePage.getElementHref(getGlobalFooterPageThreadLocal().lnkGiftCard);
+		reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,lsYmlHref),"The current Gift card href of "+lsHref+" is equal to "+lsYmlHref+ "in yml file","The current Gift card href of "+lsHref+" is not equal to "+lsYmlHref+ "in yml file");
+		lsImageSrc=basePage.getElementImageSrc(getGlobalFooterPageThreadLocal().imgGiftCard);
+		reporter.softAssert(!lsImageSrc.isEmpty(),"The Gift card image is not empty.","The Gift card image is empty.");
+		
+		//Send us feedback
+		lsText=basePage.getElementText(getGlobalFooterPageThreadLocal().lnkSendUsFeedback);		
+		String lsText_SendUsFeedback_En=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsText_SendUsFeedback_Fr);
+		reporter.softAssert(lsText.equalsIgnoreCase(lsText_SendUsFeedback_En),"The English text of "+lsText+" is correct","The English text of "+lsText+" is not correct");		
+		lsYmlHref=getGlobalFooterPageThreadLocal().getLinkWithSpecificName(lstNameAndLinks,lsText,true);
+		if(lsYmlHref.isEmpty()) {
+			reporter.reportLog("Unable to find Send us feedback link in yml file.");
+		}
+		lsHref=basePage.getElementHref(getGlobalFooterPageThreadLocal().lnkSendUsFeedback);
+		reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,lsYmlHref),"The current Send us feedback href of "+lsHref+" contains "+lsYmlHref+ "in yml file","The current Send us feedback href of "+lsHref+" does not contain "+lsYmlHref+ "in yml file");
+		lsImageSrc=basePage.getElementImageSrc(getGlobalFooterPageThreadLocal().imgSendUsFeedback);
+		reporter.softAssert(!lsImageSrc.isEmpty(),"The Send us feedback image is not empty.","The Send us feedback image is empty.");
+		
+		//Language switch
+		lsText=basePage.getUTFEnabledData(basePage.getElementText(getGlobalFooterPageThreadLocal().lnkLanguage));		
+		String lsText_LanguageSwitch_Fr=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsText_LanguageSwitch_En);
+		reporter.softAssert(lsText.equalsIgnoreCase(lsText_LanguageSwitch_Fr),"The English text of "+lsText+" is correct","The English text of "+lsText+" is not correct");			
+		lsYmlHref=getGlobalFooterPageThreadLocal().getLinkWithSpecificName(lstNameAndLinks,lsText,true);
+		if(lsYmlHref.isEmpty()) {
+			reporter.reportLog("Unable to find Language switch link in yml file.");
+		}
+		lsHref=basePage.getElementHref(getGlobalFooterPageThreadLocal().lnkLanguage);
+		reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,lsYmlHref),"The current Language switch href of "+lsHref+" contains "+lsYmlHref+ "in yml file","The current Language switch href of "+lsHref+" does not contain "+lsYmlHref+ "in yml file");
+				
+		//TSC customer hub links
+		lsText=basePage.getUTFEnabledData(basePage.getElementText(getGlobalFooterPageThreadLocal().lblTSCCustomerHubText));		
+		String lsText_TSCCustomerHub_En=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsText_TSCCustomerHub_Fr);
+		reporter.softAssert(lsText.equalsIgnoreCase(lsText_TSCCustomerHub_En),"The English text of "+lsText+" is correct","The English text of "+lsText+" is not correct");			
+			
+		WebElement item;
+		String lsEn;
+		for(int i=0;i<getGlobalFooterPageThreadLocal().lnkTSCCustomerHubAllLinks.size();i++) {
+			item=getGlobalFooterPageThreadLocal().lnkTSCCustomerHubAllLinks.get(i);
+			lsText=basePage.getUTFEnabledData(basePage.getElementText(item));	
+			lsFr=lstCustomerHubFr.get(i);			
+			lsEn=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsFr);
+			reporter.softAssert(lsText.equalsIgnoreCase(lsEn),"The English text of "+lsText+" is correct","The English text of "+lsText+" is different from "+lsEn);
+			lsYmlHref=getGlobalFooterPageThreadLocal().getLinkWithSpecificName(lstNameAndLinks,lsText,true);			
+			if(lsYmlHref.isEmpty()) {
+				reporter.reportLog("Unable to find "+lsText+" link in yml file.");
+			}
+			lsHref=basePage.getElementHref(item);	
+			reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,lsYmlHref),"The current "+lsText+" href of "+lsHref+" is correct while compared to "+lsYmlHref+ "in yml file","The current "+lsText+" href of "+lsHref+" is not correct while compared to "+lsYmlHref+ "in yml file");		
+		}
+		
+		//About TSC links
+		lsText=basePage.getUTFEnabledData(basePage.getElementText(getGlobalFooterPageThreadLocal().lblAboutTSCText));		
+		String lsText_AboutTSC_En=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsText_AboutTSC_Fr);
+		reporter.softAssert(lsText.equalsIgnoreCase(lsText_AboutTSC_En),"The English text of "+lsText+" is correct","The English text of "+lsText+" is not correct");			
+				
+		for(int i=0;i<getGlobalFooterPageThreadLocal().lnkAboutTSCAllLinks.size();i++) {
+			item=getGlobalFooterPageThreadLocal().lnkAboutTSCAllLinks.get(i);
+			lsText=basePage.getUTFEnabledData(basePage.getElementText(item));
+			lsFr=lstAboutTSCFr.get(i);
+			lsEn=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsFr);
+			reporter.softAssert(lsText.equalsIgnoreCase(lsEn),"The English text of "+lsText+" is correct","The English text of "+lsText+" is different from "+lsEn);
+			lsYmlHref=getGlobalFooterPageThreadLocal().getLinkWithSpecificName(lstNameAndLinks,lsText,true);			
+			if(lsYmlHref.isEmpty()) {
+				reporter.reportLog("Unable to find "+lsText+" link in yml file.");
+			}
+			lsHref=basePage.getElementHref(item);	
+			reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,lsYmlHref),"The current "+lsText+" href of "+lsHref+" is correct while compared to "+lsYmlHref+ "in yml file","The current "+lsText+" href of "+lsHref+" is not correct while compared to "+lsYmlHref+ "in yml file");			
+		}
+		
+		//Rogers LOGO
+		reporter.softAssert(getGlobalFooterPageThreadLocal().verifyElementExisting(getGlobalFooterPageThreadLocal().imgRogersLogo), "Rogers Logo is existing", "Rogers Logo is not existing");		
+		
+		//Copyright text
+		lsText=basePage.getUTFEnabledData(basePage.getElementText(getGlobalFooterPageThreadLocal().txtCopyrightLine1));		
+		String lsText_Copyright_Line1_En=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsText_Copyright_Line1_Fr);
+		reporter.softAssert(lsText.equalsIgnoreCase(lsText_Copyright_Line1_En),"The English text of "+lsText+" is correct","The English text of "+lsText+" is not correct");		
+		lsText=basePage.getUTFEnabledData(basePage.getElementText(getGlobalFooterPageThreadLocal().txtCopyrightLine2));	
+		String lsText_Copyright_Line2_En=getGlobalFooterPageThreadLocal().getEnglishWithSpecificFrenchName(lstNameAndLinks,lsText_Copyright_Line2_Fr);
+		reporter.softAssert(lsText.equalsIgnoreCase(lsText_Copyright_Line2_En),"The English text of "+lsText+" is correct","The English text of "+lsText+" is not correct");			
 		
 	}
 
