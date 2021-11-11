b26cada21720156c0a8b611b0156eb0cc3c08b50
Viswas.Reddy <viswas.reddy@rci.rogers.com>
2021-11-11 07:51:56 -0500

    small change for baseTest, build gradle

--- src/test/java/com/tsc/test/base/BaseTest.java
+++ src/test/java/com/tsc/test/base/BaseTest.java
@@ -145,19 +145,19 @@ public class BaseTest {
 		strBrowser=System.getProperty("Browser").trim();
 		System.out.println(strBrowser);
 
-		String lsTestDevice=System.getProperty("chromeMobileDevice").trim();
+		String lsTestDevice=System.getProperty("Device").trim();
 		TestDeviceThreadLocal.set(lsTestDevice);
 		strBrowser=System.getProperty("Browser").trim();
 		if (strBrowser.toLowerCase().contains("android") || strBrowser.toLowerCase().contains("ios")
 				|| strBrowser.toLowerCase().contains("mobile")) {
-			lsTestDevice=System.getProperty("chromeMobileDevice").trim();
+			lsTestDevice=System.getProperty("Device").trim();
 			TestDeviceThreadLocal.set(lsTestDevice);
 			switch(lsTestDevice) {
-				case "iPad":
+				case "Tablet":
 					//getDriver().manage().window().setSize(new Dimension(800, 600));
 					productResultsPageThreadLocal.set(new ProductResultsPage_Tablet(getDriver()));
 					break;
-				case "iPhone X":
+				case "Mobile":
 					//getDriver().manage().window().setSize(new Dimension(500, 600));
 					productResultsPageThreadLocal.set(new ProductResultsPage_Mobile(getDriver()));
 					break;
