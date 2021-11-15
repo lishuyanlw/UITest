bde7e58422d3c9e796e588776de455d3d4c68a54
Sachin.Sharma6 <sachin.sharma6@rci.rogers.ca>
2021-11-15 07:42:17 -0500

    Updated Test Case GT_TC05

--- src/test/java/com/tsc/test/tests/globalFooter/GF_TC05_Verify_GlobalFooter_AboutTSC_LinksAndPageObjects.java
+++ src/test/java/com/tsc/test/tests/globalFooter/GF_TC05_Verify_GlobalFooter_AboutTSC_LinksAndPageObjects.java
@@ -1,89 +1,85 @@
 package com.tsc.test.tests.globalFooter;
 
-import org.testng.annotations.Test;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.tsc.data.Handler.TestDataHandler;
 import com.tsc.pages.base.BasePage;
 import com.tsc.test.base.BaseTest;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class GF_TC05_Verify_GlobalFooter_AboutTSC_LinksAndPageObjects extends BaseTest {
-	/*
-	 * CER-184
-	 * CER-185
-	 * CER-186
-	 * CER-187
-	 * CER-188
-	 * CER-189
-	 * CER-190
-	 */
-	@Test(groups={"Home","Regression","GlobalFooter"})
-	public void Verify_GlobalFooter_LinksAndPageObjects() throws IOException {
-		getGlobalFooterPageThreadLocal().closePopupDialog();
-		BasePage basePage=new BasePage(this.getDriver());		
-		String lsBaseUrl=basePage.getBaseURL()+"/";
-		reporter.softAssert(getglobalheaderPageThreadLocal().validateURL(lsBaseUrl), "TSC url is correct", "TSC url is incorrect");		
-		reporter.reportLog("Global Footer Section");		
 
-		List<List<String>> lstNameAndLinks=TestDataHandler.constantData.getFooterSection().getLst_NameAndLinks();
-		//String lsService;
-		ArrayList<WebElement> elementList=new ArrayList<>();
-		List<String> global_footer_items = new ArrayList<>();
-		global_footer_items.add("Terms Of Use");
-		global_footer_items.add("Privacy Policy");
-		global_footer_items.add("Become a Vendor");
-		global_footer_items.add("More About TSC");
-		for(String lsService:global_footer_items) {
+    @Test(groups={"Home","Regression","GlobalFooter"})
+    public void verify_GlobalFooter_AboutTSC() {
+        getGlobalFooterPageThreadLocal().closePopupDialog();
+        BasePage basePage = new BasePage(this.getDriver());
+        String lsBaseUrl = basePage.getBaseURL() + "/";
 
-		GF_TC05_Verify_GlobalFooter_AboutTSC_LinksAndPageObjects.verifyTitle(basePage,lstNameAndLinks,lsService);
-        if(!getGlobalFooterPageThreadLocal().goToService(lsService,getGlobalFooterPageThreadLocal().lblTermsOfUseAboutOurService)) {
-				reporter.reportLogFail("Unable to navigate to '"+lsService+"' page objects.");
-			}
-			else {	
-				GF_TC05_Verify_GlobalFooter_AboutTSC_LinksAndPageObjects.verifyExpandItemAndElementList(elementList, lsService);
-				
-				//verifying More About TSC Module
-				if(lsService.trim().equalsIgnoreCase(("More About TSC").trim())) {
-					List<WebElement> elements=getGlobalFooterPageThreadLocal().lstMoreAboutTSCLinks;
-					for(int i=1;i<=elements.size();i++) {
-						getGlobalFooterPageThreadLocal().clickOnTSCOptionLink(i);
-						GF_TC05_Verify_GlobalFooter_AboutTSC_LinksAndPageObjects.verifyExpandItemAndElementList(elementList, lsService);
-						elementList.clear();
-						getGlobalFooterPageThreadLocal().goToService(lsService,getGlobalFooterPageThreadLocal().lblTermsOfUseAboutOurService);
-					}
-				}
-			}
-			elementList.clear();
-		}
-	}
+        reporter.softAssert(getglobalheaderPageThreadLocal().validateURL(lsBaseUrl), "TSC url is correct", "TSC url is incorrect");
+        reporter.reportLog("Global Footer Section");
 
-	//Verifying the Href with page link
-	public static void verifyTitle(BasePage basePage,List<List<String>> lstNameAndLinks,String lsService) {
-		reporter.reportLog(lsService);
-		WebElement selectedItem=getGlobalFooterPageThreadLocal().getServiceWebElement(lsService);		
-		String lsHref=basePage.getElementHref(selectedItem);		
-		String lsYmlHref=getGlobalFooterPageThreadLocal().getLinkWithSpecificName(lstNameAndLinks,lsService,true);
-		if(lsYmlHref.isEmpty()) {
-			reporter.reportLogFail("Unable to find '"+lsService+"' link.");
-		}		
-		reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,lsYmlHref),"The current '"+lsService+"' href of "+lsHref+" is equal to "+lsYmlHref,"The current '"+lsService+"' href of "+lsHref+" is not equal to "+lsYmlHref);
+        List<List<String>> lstNameAndLinks = TestDataHandler.constantData.getFooterSection().getLst_NameAndLinks();
 
-	}
-	//Verifying Default page section titles and contents
-	public static void verifyExpandItemAndElementList(ArrayList<WebElement> elementList,String lsService) {
-        elementList.add(getGlobalFooterPageThreadLocal().lblTermsOfUseAboutOurService);
-        //Add item section titles in Default Page Section
-		for(WebElement item:getGlobalFooterPageThreadLocal().lstOfExpandedTitle) {
-			elementList.add(item);
-		}
-        //Add item section contents in Default Page Section 
-		for(WebElement item:getGlobalFooterPageThreadLocal().lstOfExpandedContent) {
-			elementList.add(item);
-		}
-        getGlobalFooterPageThreadLocal().verifyElementListExistence(elementList);
+        List<String> global_footer_items = new ArrayList<>();
+        //Adding links present in About TSC to be verified
+        global_footer_items.add("Terms of Use");
+        global_footer_items.add("Privacy Policy");
+        global_footer_items.add("Become a Vendor");
+        for (String lsService : global_footer_items) {
+            reporter.reportLog(lsService);
+            WebElement selectedItem=getGlobalFooterPageThreadLocal().getServiceWebElement(lsService);
+            String lsHref=basePage.getElementHref(selectedItem);
+            HashMap<String,String> testData=getGlobalFooterPageThreadLocal().getTestDataWithSpecificName(lstNameAndLinks,lsService,true);
+            if(testData.isEmpty()) {
+                reporter.reportLogFail("Unable to find '"+lsService+"' link.");
+            }
+            reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,testData.get("Link")),"The current '"+lsService+"' href of "+lsHref+" is equal to "+testData.get("Link"),"The current '"+lsService+"' href of "+lsHref+" is not equal to "+testData.get("Link"));
 
-	}
+            if(!getGlobalFooterPageThreadLocal().goToService(lsService,getGlobalFooterPageThreadLocal().aboutUsPageTitle)) {
+                reporter.reportLogFail("Unable to navigate to '"+lsService+"' page objects.");
+            }
+            else {
+                //Verifying page title
+                String pageTitle = getGlobalFooterPageThreadLocal().getPageTitle(getGlobalFooterPageThreadLocal().aboutUsPageTitle);
+                reporter.softAssert(pageTitle.equalsIgnoreCase(testData.get("Title")),"Page Title matches for global footer link: "+lsService+" and title is: "+pageTitle,"Page Title doesn't match for global footer link: "+lsService+" and title is: "+pageTitle);
+                //Verifying that user is navigated to respective section after navigating to page
+                Boolean respectiveSectionValue=getGlobalFooterPageThreadLocal().verifyRespectiveSectionForLinkOnPage(getGlobalFooterPageThreadLocal().lstMyAccountItemTitle);
+                reporter.softAssert(respectiveSectionValue,"Section for "+lsService+" on page: "+pageTitle+" is open after navigating","Section for "+lsService+" on page: "+pageTitle+" is not open after navigating");
+                //Verifying Page Layout and sections
+                getGlobalFooterPageThreadLocal().expandPanelItems(getGlobalFooterPageThreadLocal().lstMyAccountItemTitle,getGlobalFooterPageThreadLocal().lstMyAccountItemContent);
+            }
+        }
+
+        //More About TSC
+        String lsService = "More About TSC";
+        ArrayList<WebElement> elementList=new ArrayList<WebElement>();
+        reporter.reportLog(lsService);
+        WebElement selectedItem=getGlobalFooterPageThreadLocal().getServiceWebElement(lsService);
+        String lsHref=basePage.getElementHref(selectedItem);
+        HashMap<String,String> testData=getGlobalFooterPageThreadLocal().getTestDataWithSpecificName(lstNameAndLinks,lsService,true);
+        if(testData.isEmpty()) {
+            reporter.reportLogFail("Unable to find '"+lsService+"' link.");
+        }
+        reporter.softAssert(getGlobalFooterPageThreadLocal().verifyLinks(lsHref,testData.get("Link")),"The current '"+lsService+"' href of "+lsHref+" is equal to "+testData.get("Link"),"The current '"+lsService+"' href of "+lsHref+" is not equal to "+testData.get("Link"));
+
+        if(!getGlobalFooterPageThreadLocal().goToService(lsService,getGlobalFooterPageThreadLocal().aboutUsPageTitle)) {
+            reporter.reportLogFail("Unable to navigate to '"+lsService+"' page objects.");
+        }
+        else {
+            //Add In Page sub Header values
+            for(WebElement item:getGlobalFooterPageThreadLocal().subHeaders) {
+                elementList.add(item);
+            }
+
+            //Add Links present under different sub Headers on page
+            for(WebElement item:getGlobalFooterPageThreadLocal().subHeaderLinks) {
+                elementList.add(item);
+            }
+
+            getGlobalFooterPageThreadLocal().verifyElementListExistence(elementList);
+        }
+    }
 }
