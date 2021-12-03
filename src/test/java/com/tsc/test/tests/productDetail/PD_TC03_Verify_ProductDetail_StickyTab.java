6b61fccd5cebe1d8f1e8182c16967aaa64c90e9a
Wei Li - EXT <Wei.Li@rci.rogers.ca>
2021-12-03 13:36:26 -0500

    Small bug fixing

--- src/test/java/com/tsc/test/tests/productDetail/PD_TC03_Verify_ProductDetail_StickyTab.java
+++ src/test/java/com/tsc/test/tests/productDetail/PD_TC03_Verify_ProductDetail_StickyTab.java
@@ -47,7 +47,7 @@ public class PD_TC03_Verify_ProductDetail_StickyTab extends BaseTest{
 			reporter.reportLog("Review tab review list contents");
 			getProductDetailPageThreadLocal().verifyReviewTabPerReviewListContents();
 			String lsMsg=getProductDetailPageThreadLocal().checkReviewRateSortingBy(true);
-			if(lsMsg.isEmpty()) {
+			if(lsMsg.isEmpty()||lsMsg.contains("less than 2")) {
 				reporter.reportLogPass("Sorting by Highest rated passed!");
 			}
 			else {
@@ -56,7 +56,7 @@ public class PD_TC03_Verify_ProductDetail_StickyTab extends BaseTest{
 			
 			getProductDetailPageThreadLocal().chooseReviewSortingOption("Lowest Rated");
 			lsMsg=getProductDetailPageThreadLocal().checkReviewRateSortingBy(false);
-			if(lsMsg.isEmpty()) {
+			if(lsMsg.isEmpty()||lsMsg.contains("less than 2")) {
 				reporter.reportLogPass("Sorting by Lowest rated passed!");
 			}
 			else {
