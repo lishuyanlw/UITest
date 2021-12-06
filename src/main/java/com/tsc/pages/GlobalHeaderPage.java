8a9ed5bddd91d43dff97d762e3e403b443fbe515
sdrogers1 <Shruti.Desai1@rci.rogers.com>
2021-12-06 00:16:51 -0500

    add waitfor condition for TC01

--- src/main/java/com/tsc/pages/GlobalHeaderPage.java
+++ src/main/java/com/tsc/pages/GlobalHeaderPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
@@ -49,6 +50,10 @@ public class GlobalHeaderPage extends BasePage{
 	
 	@FindBy(xpath = "//div[contains(@class,'black-header')]//nav//li//a[contains(@href,'OnAir')]")
 	public WebElement lnkOnAirProductsDpdMenu;
+	
+	@FindBy(xpath = "//h1")
+	public WebElement headingWatchTSCDpdMenuLinkLandingPage;
+
 	
 	//Sliver Links [Dynamic event, TS, Deals, OnAir, Program Guide, Watch Us Live]
 	@FindBy(xpath = "//*[@class='BlackHeader']//a[contains(@href, 'todaysshowstopper')]")
@@ -431,7 +436,9 @@ public class GlobalHeaderPage extends BasePage{
 		 
 		 blackItem.click();
 		 this.waitForPageToLoad();
-		 (new GlobalFooterPage(this.getDriver())).waitForPageLoading();
+		 waitForCondition(Driver->{return headingWatchTSCDpdMenuLinkLandingPage.isDisplayed();} ,30000);
+			
+		//(new GlobalFooterPage(this.getDriver())).waitForPageLoading();
 		 
 		 String lsUrlInSilverHeader=this.removeLastSlashFromUrl(this.URL());
 		 if(bCheckUrl) {
@@ -769,6 +776,7 @@ public class GlobalHeaderPage extends BasePage{
 	 */
 	public void verifyWatchTSCdpDMenu(WebElement element, Map<String,String> link) {
 		 getReusableActionsInstance().javascriptScrollByVisibleElement(element);
+		 
 		 String lsTitle=element.getText().trim();
 		 WebElement elementHrefWatchTSC =element.findElement(By.xpath("./ancestor::a"));
 		 reporter.softAssert(getReusableActionsInstance().isElementVisible(element), "The element of "+lsTitle+" is visible","The element of "+lsTitle+" is not visible");
@@ -778,6 +786,7 @@ public class GlobalHeaderPage extends BasePage{
 		 lsHrefWatchTSCdpMenu=this.removeLastSlashFromUrl(lsHrefWatchTSCdpMenu);
 		 element.click();
 		 this.waitForPageToLoad();
+		 waitForCondition(Driver->{return headingWatchTSCDpdMenuLinkLandingPage.isDisplayed();} ,30000);
 		 (new GlobalFooterPage(this.getDriver())).waitForPageLoading();
 		 String lsUrlWatchTSCdpItem=this.removeLastSlashFromUrl(this.URL());
 		 if(link!=null){
@@ -794,6 +803,83 @@ public class GlobalHeaderPage extends BasePage{
 			 reporter.softAssert(lsUrlWatchTSCdpItem.equalsIgnoreCase(lsHrefWatchTSCdpMenu), "The Url of "+lsUrlWatchTSCdpItem+"  after clicking "+lsTitle+" is equal to the href of "+lsHrefWatchTSCdpMenu, "The Url of "+lsUrlWatchTSCdpItem+"  after clicking "+lsTitle+" is not equal to the href of "+lsHrefWatchTSCdpMenu);
 		}
 	 }
+
+	
+	public void validateTodayShowstopperLink(List<String> testData) {
+		String nameTodayShowstopper=lnkTSBlackHeader.getText();
+		reporter.softAssert((testData.get(0).contains(nameTodayShowstopper)), "Today's Showstopper-FR text is matches with yml data file.", "Today's Showstopper-FR text is not matches with data file.");
+	}
+		
+	public void validateWatchTSCLink(List<String> testData) {
+		String nameWatchTSC=btnWatchTSCBlackHeader.getText();
+		
+		reporter.softAssert((testData.get(0).contains(nameWatchTSC)), "Today's Showstopper-FR text is matches with yml data file.", "Today's Showstopper-FR text is not matches with data file.");
+	}
+	
+	
+	//Watch TSC dropdown menu French & Link Name
+		public List<String> getFrenchName(List<List<String>> lstNameWatchTSC ){	
+			List<String> frenchNameWatchTSC = new ArrayList<>();
+				for (List<String> linkName : lstNameWatchTSC) {
+				String name=getUTFEnabledData(linkName.get(1));
+				frenchNameWatchTSC.add(name);
+				}
+			return frenchNameWatchTSC;
+		}
+		
+		public Map<String,Map<String, String>> getWatchTSCDdMenuLinkMap(List<List<String>> lstNameWatchTSC ){	
+			Map<String,Map<String, String>> linkMap = new HashMap<>();
+			for (List<String> linkName : lstNameWatchTSC) {
+				if(Boolean.valueOf(linkName.get(3))) {
+					linkMap.put(linkName.get(1),(HashMap)new HashMap<String,String>(){{put(linkName.get(0),linkName.get(2));}});
+				}
+			}
+			return linkMap;
+		}
+	public void validateWatchTSCDdMenuLinks(List<String> dDMenuLinks, List<String>frenchNameWatchTSC,WebElement element, Map<String,Map<String, String>> linkMap ) {
+		hoverOnWatchTSC();	
+		reporter.reportLog("Validating Watch TSC drop down menu.");
+		int sizeDpdMenu=lstWatchTSCDpdMenu.size();
+		reporter.softAssert(dDMenuLinks.size()==sizeDpdMenu,"Number of Watch TSC drop down menu element maches with test Data","Number of Watch TSC drop down menu elements are not maching with test Data");
+		
+		for(String frenchName:frenchNameWatchTSC) {
+			WebElement watchTSCElement=getWatchTSCdPMElements(frenchName);
+			verifyWatchTSCdpDMenu(watchTSCElement,linkMap.get(frenchName));
+			goBackHomePage();
+			hoverOnWatchTSC();	
+		}
+	}
+	public void verifyWatchTSCdpDMenu_1(String frenchName, Map<String,String> link) {	
+	
+		
+			WebElement watchTSCElement=getWatchTSCdPMElements(frenchName);
+			getReusableActionsInstance().javascriptScrollByVisibleElement(watchTSCElement);
+	 String lsTitle=watchTSCElement.getText().trim();
+	 WebElement elementHrefWatchTSC =watchTSCElement.findElement(By.xpath("./ancestor::a"));
+	 reporter.softAssert(getReusableActionsInstance().isElementVisible(watchTSCElement), "The element of "+lsTitle+" is visible","The element of "+lsTitle+" is not visible");
+	 reporter.softAssert(!lsTitle.isEmpty(), lsTitle+" text "+" is not empty", lsTitle+" text "+" is empty");
+	 String lsHrefWatchTSCdpMenu=elementHrefWatchTSC.getAttribute("href").trim();
+	 reporter.softAssert(verifyElementProperty(elementHrefWatchTSC,"Link"), "The href of "+lsTitle+" is not empty", "The href of "+lsTitle+" is empty");
+	 lsHrefWatchTSCdpMenu=this.removeLastSlashFromUrl(lsHrefWatchTSCdpMenu);
+	 watchTSCElement.click();
+	 this.waitForPageToLoad();
+	 waitForCondition(Driver->{return headingWatchTSCDpdMenuLinkLandingPage.isDisplayed();} ,30000);
+	 (new GlobalFooterPage(this.getDriver())).waitForPageLoading();
+	 String lsUrlWatchTSCdpItem=this.removeLastSlashFromUrl(this.URL());
+	 if(link!=null){
+		 for (Map.Entry mapElement : link.entrySet()) {
+			 if(lsHrefWatchTSCdpMenu.contains(mapElement.getKey().toString())) {
+				 String baseURL = System.getProperty("QAUrl")+'/'+mapElement.getValue();
+				 reporter.softAssert(lsUrlWatchTSCdpItem.equalsIgnoreCase(baseURL), "The Url of "+lsUrlWatchTSCdpItem+"  after clicking "+lsTitle+" is equal to the href of "+lsHrefWatchTSCdpMenu, "The Url of "+lsUrlWatchTSCdpItem+"  after clicking "+lsTitle+" is not equal to the href of "+lsHrefWatchTSCdpMenu);
+			 }else{
+			 	reporter.softAssert(false,"","URL for "+mapElement.getKey().toString()+" is not verified that should be as passed as input parameter");
+			 }
+		 }
+	 }
+	 else {
+		 reporter.softAssert(lsUrlWatchTSCdpItem.equalsIgnoreCase(lsHrefWatchTSCdpMenu), "The Url of "+lsUrlWatchTSCdpItem+"  after clicking "+lsTitle+" is equal to the href of "+lsHrefWatchTSCdpMenu, "The Url of "+lsUrlWatchTSCdpItem+"  after clicking "+lsTitle+" is not equal to the href of "+lsHrefWatchTSCdpMenu);
+	}
+ }
 
 }
 
