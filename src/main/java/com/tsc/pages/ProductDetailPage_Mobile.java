0c21e57625615634e198b3fadffc1ba89d1be694
viswas-reddy <92472440+viswas-reddy@users.noreply.github.com>
2021-12-16 17:09:25 -0500

    changes to click on play button

--- src/main/java/com/tsc/pages/ProductDetailPage_Mobile.java
+++ src/main/java/com/tsc/pages/ProductDetailPage_Mobile.java
@@ -126,9 +126,13 @@ public class ProductDetailPage_Mobile extends ProductDetailPage{
         //this.videoIcon.click();
         applyStaticWait(2000);
         this.getReusableActionsInstance().javascriptScrollByVisibleElement(this.playButton);
-        applyStaticWait(2000);
         this.getReusableActionsInstance().scrollToElement(this.playButton);
         this.getReusableActionsInstance().clickIfAvailable(this.playButton);
+        if (!this.checkIfVideoisPlaying()){
+            this.getReusableActionsInstance().javascriptScrollByVisibleElement(this.playButton);
+            this.getReusableActionsInstance().scrollToElement(this.playButton);
+            this.getReusableActionsInstance().clickIfAvailable(this.playButton);
+        }
         //this.playButton.click();
         reporter.softAssert(this.getReusableActionsInstance().isElementVisible(this.videoBoxControl),"The video control section is displaying correctly","The video control section is not displaying correctly");
         reporter.softAssert(!this.lnkVideo.getAttribute("src").isEmpty(),"The product video source is not empty","The product video source is empty");
