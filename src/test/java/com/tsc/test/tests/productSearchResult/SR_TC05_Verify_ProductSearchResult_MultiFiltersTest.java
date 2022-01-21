ed6010ed37986598fa083e7cefd41c9b73a858d2
Wei Li - EXT <Wei.Li@rci.rogers.ca>
2022-01-21 15:42:48 -0500

    Added bug comments in PRP

--- src/test/java/com/tsc/test/tests/productSearchResult/SR_TC05_Verify_ProductSearchResult_MultiFiltersTest.java
+++ src/test/java/com/tsc/test/tests/productSearchResult/SR_TC05_Verify_ProductSearchResult_MultiFiltersTest.java
@@ -16,7 +16,6 @@ public class SR_TC05_Verify_ProductSearchResult_MultiFiltersTest extends BaseTes
 	 * CER-225
 	 * CER-226
 	 * Bug-19685 - Reviews filter doesn't display stars in filter - covered in selectFilterItemInLeftPanel() function
-	 * Bug 19628: [QA Defect - P3] PRP: no products display if user is on the last page and select a faucet from the left nav - covered in selectFilterItemInLeftPanel() function
 	 */
 	@Test(groups={"ProductSearch","Regression","Regression_Tablet","Regression_Mobile"})
 	public void validateProductSearchResult_MultiFiltersFunction() throws IOException {
