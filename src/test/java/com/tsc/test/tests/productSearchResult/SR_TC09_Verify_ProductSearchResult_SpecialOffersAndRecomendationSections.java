3d6fe011edb902a4d52561799efc04434f5008c4
Sachin.Sharma6 <sachin.sharma6@rci.rogers.ca>
2022-01-27 12:55:46 -0500

    Updated function to click on Header menu due to xpath change

--- src/test/java/com/tsc/test/tests/productSearchResult/SR_TC09_Verify_ProductSearchResult_SpecialOffersAndRecomendationSections.java
+++ src/test/java/com/tsc/test/tests/productSearchResult/SR_TC09_Verify_ProductSearchResult_SpecialOffersAndRecomendationSections.java
@@ -25,7 +25,8 @@ public class SR_TC09_Verify_ProductSearchResult_SpecialOffersAndRecomendationSec
 		List<WebElement> productList;
 
 		// Corresponding actions (Clearance>>Beauty)
-		String subMenuItem = getglobalheaderPageThreadLocal().getNameAndclickSubMenuItem("Clearance","Electronics","Speakers & Audio");
+		//String subMenuItem = getglobalheaderPageThreadLocal().getNameAndclickSubMenuItem("Clearance","Electronics","Speakers & Audio");
+		String subMenuItem = getglobalheaderPageThreadLocal().getNameAndclickSubMenuItem("Clearance","Beauty",null);
 
 		// Verifying that landing page is product results page after navigation
 		reporter.softAssert(getProductResultsPageThreadLocal().getClearanceOptionURLTitle().contains(lnkProductResult),"Verified that landing page is Product Result Page", "Verified that landing page is not Product Result Page");
