ab9ce23f9ef9a146c1cb5408416a5deb4902988c
Sachin.Sharma6 <sachin.sharma6@rci.rogers.ca>
2022-02-04 21:09:46 -0500

    Updated Global Header test case

--- src/main/java/com/tsc/pages/LoginPage_Mobile.java
+++ src/main/java/com/tsc/pages/LoginPage_Mobile.java
@@ -62,11 +62,12 @@ public class LoginPage_Mobile extends LoginPage{
     @Override
     public boolean Login(String lsUserName, String lsPassword,String lsFirstName) {
         getReusableActionsInstance().javascriptScrollByVisibleElement(this.SigninIcon);
-        this.SigninIcon.click();
+        getReusableActionsInstance().staticWait(3000);
+        getReusableActionsInstance().clickIfAvailable(this.SigninIcon);
         getReusableActionsInstance().staticWait(2000);
         getReusableActionsInstance().javascriptScrollByVisibleElement(this.btnSignInMainMenu);
         getReusableActionsInstance().scrollToElement(this.btnSignInMainMenu);
-        getReusableActionsInstance().staticWait(300);
+        getReusableActionsInstance().staticWait(2000);
         getReusableActionsInstance().javascriptScrollByVisibleElement(this.btnSignInNav);
         this.btnSignInNav.click();
         (new GlobalFooterPage(this.getDriver())).waitForPageLoading();
@@ -78,6 +79,9 @@ public class LoginPage_Mobile extends LoginPage{
         String lsSignInMsg=this.btnSignInMainMenu.getText();
         getReusableActionsInstance().javascriptScrollByVisibleElement(this.btnSubmit);
         this.btnSubmit.click();
+
+        waitForCondition(Driver->{return this.lblSignInPageTitle.isDisplayed();},30000);
+        getReusableActionsInstance().staticWait(2000);
 
         return waitForCondition(Driver->{return !lsSignInMsg.equalsIgnoreCase(this.btnSignInMainMenu.getText());},30000);
         //return super.Login(String lsUserName, String lsPassword);
