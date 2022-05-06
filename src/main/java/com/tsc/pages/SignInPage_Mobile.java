0811dc28c27cd70ee603eca16946ba8cd74d5048
Wei Li - EXT <Wei.Li@rci.rogers.ca>
2022-05-06 13:37:03 -0400

    Refactored LogIn function

--- src/main/java/com/tsc/pages/SignInPage_Mobile.java
+++ src/main/java/com/tsc/pages/SignInPage_Mobile.java
@@ -54,46 +54,6 @@ public class SignInPage_Mobile extends SignInPage{
         }
     }
 
-   /** @Override
-    public void verifySignInSection() {
-        getReusableActionsInstance().javascriptScrollByVisibleElement(this.SigninIcon);
-        this.SigninIcon.click();
-        getReusableActionsInstance().staticWait(2000);
-        //getReusableActionsInstance().javascriptScrollByVisibleElement(this.cntSignInPopover);
-        super.verifySignInSection();
-        /*getReusableActionsInstance().javascriptScrollByVisibleElement(this.btnSignInNav);
-        this.btnSignInNav.click();
-        (new GlobalFooterPage(this.getDriver())).waitForPageLoading();
-        reporter.softAssert(getReusableActionsInstance().isElementVisible(this.inputUserName),"Input field is existing","Input field is not existing");
-        reporter.softAssert(getReusableActionsInstance().isElementVisible(this.inputPassword),"Password field is existing","Password field is not existing");*//*
-    }*/
-
-    @Override
-    public boolean Login(String lsUserName, String lsPassword) {
-        getReusableActionsInstance().javascriptScrollByVisibleElement(this.cntBlackHeaderContainer);
-        getReusableActionsInstance().staticWait(2000);
-
-        this.clickElement(this.btnSignInMainMenu);
-        getReusableActionsInstance().staticWait(2000);
-
-        this.btnSignInNav.click();
-        (new GlobalFooterPage(this.getDriver())).waitForPageLoading();
-        getReusableActionsInstance().javascriptScrollByVisibleElement(this.inputUserName);
-        this.inputUserName.sendKeys(lsUserName);
-        getReusableActionsInstance().javascriptScrollByVisibleElement(this.inputPassword);
-        this.inputPassword.sendKeys(lsPassword);
-
-        String lsSignInMsg=this.btnSignInMainMenu.getText();
-        getReusableActionsInstance().javascriptScrollByVisibleElement(this.btnSubmit);
-        this.btnSubmit.click();
-
-        waitForCondition(Driver->{return this.lblSignInPageTitle.isDisplayed();},30000);
-        getReusableActionsInstance().staticWait(2000);
-
-        return waitForCondition(Driver->{return !lsSignInMsg.equalsIgnoreCase(this.btnSignInMainMenu.getText());},30000);
-        //return super.Login(String lsUserName, String lsPassword);
-    }
-
     @Override
     public void verifyConfidence(){
         this.getReusableActionsInstance().javascriptScrollByVisibleElement(this.lblConfidence);
