6247da38dff3a344d5f0136a533cad81b88a3771
Sachin.Sharma6 <sachin.sharma6@rci.rogers.ca>
2022-08-04 13:00:28 -0400

    Removed un-necessary imports from test case SC_TC09_VerifyShoppingCart_Free_Gift_Item.java

--- src/test/java/com/tsc/test/tests/shoppingCart/SC_TC09_VerifyShoppingCart_Free_Gift_Item.java
+++ src/test/java/com/tsc/test/tests/shoppingCart/SC_TC09_VerifyShoppingCart_Free_Gift_Item.java
@@ -6,7 +6,6 @@ import com.tsc.api.pojo.Configuration;
 import com.tsc.api.pojo.Product;
 import com.tsc.api.util.DataConverter;
 import com.tsc.data.Handler.TestDataHandler;
-import com.tsc.pages.base.BasePage;
 import com.tsc.test.base.BaseTest;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
