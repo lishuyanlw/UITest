37772c7268fdc951f2253f86bfc80ed6edd20b65
Sachin.Sharma6 <sachin.sharma6@rci.rogers.ca>
2022-08-23 08:28:14 -0400

    Updated ShoppingCartPage_Mobile.java for PayPal Test

--- src/main/java/com/tsc/pages/ShoppingCartPage_Mobile.java
+++ src/main/java/com/tsc/pages/ShoppingCartPage_Mobile.java
@@ -7,10 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ShoppingCartPage_Mobile extends ShoppingCartPage {
 
@@ -747,5 +744,53 @@ public class ShoppingCartPage_Mobile extends ShoppingCartPage {
 			reporter.reportLog("Verification for Blue Jays Foundation is done as expected");
 		else
 			reporter.reportLogFailWithScreenshot("Blue Jays Foundation verification is not done!!");
+	}
+
+	@Override
+	public void verifyPayPalPopUpExistenceOnClick(){
+		boolean flag = false;
+		Set<String> parentWindowHandle = this.getDriver().getWindowHandles();
+		List<String> list = new ArrayList<String>(parentWindowHandle);
+		//Switch to PayPal frame
+		this.getDriver().switchTo().frame(framePayPalFrameElement);
+
+		this.getReusableActionsInstance().javascriptScrollByVisibleElement(this.btnPayPalButton);
+		this.getReusableActionsInstance().clickIfAvailable(this.btnPayPalButton);
+		this.waitForCondition(Driver->{return this.getDriver().getWindowHandles().size()>1;},5000);
+		Set<String> windowHandles = this.getDriver().getWindowHandles();
+		System.out.println(parentWindowHandle);
+		System.out.println(windowHandles);
+		if(windowHandles.size()>1){
+			for(String windowHandle:windowHandles){
+				System.out.println(windowHandle+" : "+list.get(0));
+				if(!windowHandle.equalsIgnoreCase(list.get(0))){
+					flag = true;
+					this.getDriver().switchTo().window(windowHandle);
+					this.waitForCondition(Driver->{return this.getReusableActionsInstance().isElementVisible(this.inputPayPalEmailInput) && this.inputPayPalEmailInput.isEnabled();},10000);
+					String payPalUrl = this.getDriver().getCurrentUrl();
+					if(payPalUrl.contains("paypal.com"))
+						reporter.reportLogPass("User is navigated to PayPal pop up as expected");
+					else
+						reporter.reportLogFail("User is not navigated to PayPal pop up as expected with url: "+payPalUrl);
+
+					//Verification of email input box
+					if(this.getReusableActionsInstance().isElementVisible(this.inputPayPalEmailInput) && this.inputPayPalEmailInput.isEnabled())
+						reporter.reportLog("Email Input on Pay Pal Pop Up is enabled");
+					else
+						reporter.reportLogFailWithScreenshot("Email input on Pay Pal pop up is either not displayed or not enabled");
+
+					this.getDriver().close();
+				}
+				if(flag){
+					this.getReusableActionsInstance().switchToMainWindow(list.get(0));
+					break;
+				}
+			}
+			if(flag)
+				reporter.reportLogPass("Verification for pay pal pop is done");
+			else
+				reporter.reportLogFailWithScreenshot("Verification for pay pal pop is not done as expected!");
+		}else
+			reporter.reportLogFailWithScreenshot("Pay Pal pop up is not displayed as expected");
 	}
 }
