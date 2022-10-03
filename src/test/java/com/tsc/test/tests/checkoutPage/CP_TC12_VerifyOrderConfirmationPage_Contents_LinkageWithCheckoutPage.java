9d8f794b87497810a3f1404cbc5b00036d3084b9
Wei Li - EXT <Wei.Li@rci.rogers.ca>
2022-10-03 15:47:52 -0400

    Refactored codes for CP_TC12_VerifyOrderConfirmationPage_Contents_LinkageWithCheckoutPage

--- src/test/java/com/tsc/test/tests/checkoutPage/CP_TC12_VerifyOrderConfirmationPage_Contents_LinkageWithCheckoutPage.java
+++ src/test/java/com/tsc/test/tests/checkoutPage/CP_TC12_VerifyOrderConfirmationPage_Contents_LinkageWithCheckoutPage.java
@@ -36,12 +36,12 @@ public class CP_TC12_VerifyOrderConfirmationPage_Contents_LinkageWithCheckoutPag
 		getShoppingCartThreadLocal().verifyCartExistsForUser(Integer.valueOf(customerEDP), accessToken, keyword,true);
 
 		//To add advanced order Product
-//		String lsKeyword=TestDataHandler.constantData.getSearchResultPage().getLbl_AdvancedOrderkeyword();
-//		Map<String,Object> mapAPI=getShoppingCartThreadLocal().addSingleProductWithConditions(lsKeyword, 1,1, String.valueOf(customerEDP), accessToken,true);
+		String lsKeyword=TestDataHandler.constantData.getSearchResultPage().getLbl_AdvancedOrderkeyword();
+		Map<String,Object> mapAPI=getShoppingCartThreadLocal().addSingleProductWithConditions(lsKeyword, 1,1, String.valueOf(customerEDP), accessToken,true);
 
 		//To add auto delivery order Product
-//		lsKeyword=TestDataHandler.constantData.getSearchResultPage().getLbl_AutoDeliverykeyword();
-//		mapAPI=getShoppingCartThreadLocal().addSingleProductWithConditions(lsKeyword, 1,1, String.valueOf(customerEDP), accessToken,false);
+		lsKeyword=TestDataHandler.constantData.getSearchResultPage().getLbl_AutoDeliverykeyword();
+		mapAPI=getShoppingCartThreadLocal().addSingleProductWithConditions(lsKeyword, 1,1, String.valueOf(customerEDP), accessToken,false);
 
 		//Delete promote code and all gift cards
 		CartAPI cartAPI=new CartAPI();
